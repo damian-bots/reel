7523a7980af8370d36798ddde4d3df75f842f81e
damian-bots <tosu0791@gmail.com>
2025-02-18 01:52:40 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -7,7 +7,7 @@ API_ID = "24620300"
 API_HASH = "9a098f01aa56c836f2e34aee4b7ef963"
 BOT_TOKEN = "8001341321:AAGF8SbLP-JBr5rTC6j_J6bZfRR6L8r0OQo"
 
-bot = Client("insta_reels_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+bot = Client("DeadlineReelbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 loader = instaloader.Instaloader(download_video_thumbnails=False, save_metadata=False)
 
 @bot.on_message(filters.command("start"))
